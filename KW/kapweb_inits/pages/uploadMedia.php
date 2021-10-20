255bebb52f5f8c295b53d7a5c107346b702b396e
Benjamin Delvert <benjamin.delvert@epitech.eu>
2021-10-20 19:12:20 +0200

    CHG : change page if you did not send image add on page mediaUpload

--- KW/kapweb_inits/pages/uploadMedia.php
+++ KW/kapweb_inits/pages/uploadMedia.php
@@ -27,4 +27,5 @@
         }
         header("location: " . $hlp->getMainUrl() . "/KW/manager&pageBtn=btnImages&navMenu=navMenu2");
     }
+    header("location: " . $hlp->getMainUrl() . "/KW/manager&pageBtn=btnImages&navMenu=navMenu2");
 ?>
