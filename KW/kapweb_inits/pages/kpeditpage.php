f9fd084dc0fcc743a1e7d442c2f3baa82bf0846b
Benjamin Delvert <benjamin.delvert@epitech.eu>
2021-11-19 15:31:32 +0100

    CHG : when tab is pressed in css et js textarea, tab is write in text

--- KW/kapweb_inits/pages/kpeditpage.php
+++ KW/kapweb_inits/pages/kpeditpage.php
@@ -311,4 +311,36 @@
 			}
 		?>
     </body>
+	<script>
+		document.getElementById('cssEdit').addEventListener('keydown', function(e) {
+			if (e.key == 'Tab') {
+				e.preventDefault();
+				var start = this.selectionStart;
+				var end = this.selectionEnd;
+
+				// set textarea value to: text before caret + tab + text after caret
+				this.value = this.value.substring(0, start) +
+				"\t" + this.value.substring(end);
+
+				// put caret at right position again
+				this.selectionStart =
+				this.selectionEnd = start + 1;
+			}
+		});
+		document.getElementById('jsEdit').addEventListener('keydown', function(e) {
+			if (e.key == 'Tab') {
+				e.preventDefault();
+				var start = this.selectionStart;
+				var end = this.selectionEnd;
+
+				// set textarea value to: text before caret + tab + text after caret
+				this.value = this.value.substring(0, start) +
+				"\t" + this.value.substring(end);
+
+				// put caret at right position again
+				this.selectionStart =
+				this.selectionEnd = start + 1;
+			}
+		});
+	</script>
 </html>
